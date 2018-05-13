431f1bea0a30b8c97352ac0cde431ef81352c669
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-13 22:31:46 +0000

    hg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-/*const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')))
@@ -8,22 +8,5 @@ app.use(express.static(path.join(__dirname, 'public')))
   app.set('view engine', 'ejs')
   app.get('/', (req, res) => res.render('pages/index'))
   app.listen(PORT, () => console.log('Listening on ${ PORT }'))
-  */
+  
 
-
-
-res.render( 'index', {
-  title: 'Unauthenticated',
-  errorMessage: 'This app may only be loaded via Salesforce Marketing Cloud',
-})
-
-
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => res.render('pages/index2'))
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
