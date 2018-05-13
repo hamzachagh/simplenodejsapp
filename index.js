9fa48a7ae4387cc8d6e00bfea8ef8afdced2d4ca
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-13 23:11:22 +0000

    WELCOME

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')))
+  app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
   app.get('/', (req, res) => res.render('pages/index'))
+
+  app.get('/', (req, res)=>
+    res.render('pages/index',{user:"Hamza CHAGH"}) 
+  );
   app.listen(PORT, () => console.log('Listening on ${ PORT }'))
   
 
