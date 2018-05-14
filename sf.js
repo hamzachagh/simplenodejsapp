6462978c16f597bdcff40f62fd5ccab887cb1d1d
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-14 00:31:07 +0000

    dd

--- sf.js
+++ sf.js
@@ -4,7 +4,7 @@ var nforce = require('nforce');
 var org = nforce.createConnection({
   clientId: '3MVG9Rd3qC6oMalXcM8hVxjOJvhYFvfY_wIr99DZ23Y_nlfVWpJsw1bRzL_eKt6E31PskZRiL5sgdbXsYA3s.',
   clientSecret: '8971956947311806869',
-  redirectUri: 'http://localhost:5000/oauth/_callback',
+  redirectUri: 'http://localhost:3000/oauth/_callback',
   apiVersion: 'v42.0',  // optional, defaults to current salesforce API version
   environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
   mode: 'single', // optional, 'single' or 'multi' user mode, multi default
@@ -16,11 +16,19 @@ var org = nforce.createConnection({
 
 ///single-user mode
 org.authenticate({ username: 'hamzachagh2@gmail.com', password: 'Hamza_SE2018'}, function(err, resp){
-  if(!err) console.log('Successfully connected to Salesforce. Cached token: ' + org.oauth.access_token);
-  if(err) console.log('Cannot connect to Salesforce: ' + err);
+    // the oauth object was stored in the connection object
+    //if(!err) console.log('Cached Token: ' + org.oauth.access_token)
+
+    org.query({ query: 'SELECT Id, name FROM Lead limit 1 ' }, function(err, res) {
+        if(err) return console.error(err);
+        else 
+         
+        console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ '+res.records);
+        
+
+      })
 });
 
 
 
-
-module.exports = org;
+module.exports.org  = org;
