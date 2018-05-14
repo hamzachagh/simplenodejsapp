cbb558424119cb817a90aa689a6adb07da6e9fe1
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-14 00:16:24 +0000

    sssqsqs

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ var app = express();
 
     org.query({ query: "SELECT Id, name FROM Lead limit 1" })
       .then(function(results){
-        res.render('pages/lead', { records: results.records });
+        res.render('pages/lead', { records: [
+          { name: 'Bloody Mary', drunkness: 3 },
+          { name: 'Martini', drunkness: 5 },
+          { name: 'Scotch', drunkness: 10 }
+      ] });
       });
   
   });
