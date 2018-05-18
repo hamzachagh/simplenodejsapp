564c95b324d5b33826082df36e85f7ecf408f1e9
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-18 10:50:17 +0000

    sss

--- sf.js
+++ sf.js
@@ -1,11 +1,22 @@
 var nforce = require('nforce');
 
 
-var org = nforce.createConnection({
+/* var org = nforce.createConnection({
   clientId: '3MVG9Rd3qC6oMalXcM8hVxjOJvhYFvfY_wIr99DZ23Y_nlfVWpJsw1bRzL_eKt6E31PskZRiL5sgdbXsYA3s.',
   clientSecret: '8971956947311806869',
   redirectUri: 'http://localhost:3000/oauth/_callback',
-  apiVersion: 'v42.0',  // optional, defaults to current salesforce API version
+  apiVersion: 'v41.0',  // optional, defaults to current salesforce API version
+  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'single', // optional, 'single' or 'multi' user mode, multi default
+  autoRefresh: true
+}); */
+
+
+var org = nforce.createConnection({
+  clientId: '3MVG95NPsF2gwOiNLx3MoFuCQffRip3hPC7McxHmQkczbGnjSZCId1pI0oqVy28f1qI2F7ZKApJc7Padj9Cid',
+  clientSecret: '1849414258188740820',
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  apiVersion: 'v41.0',  // optional, defaults to current salesforce API version
   environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
   mode: 'single', // optional, 'single' or 'multi' user mode, multi default
   autoRefresh: true
@@ -14,12 +25,17 @@ var org = nforce.createConnection({
 
 
 
-///single-user mode
+
+/* //single-user mode
 org.authenticate({ username: 'hamzachagh2@gmail.com', password: 'Hamza_SE2018'}, function(err, resp){
   if(!err) console.log('Successfully connected to Salesforce. Cached token: ' + org.oauth.access_token);
   if(err) console.log('Cannot connect to Salesforce: ' + err);
-});
+}); */
 
+org.authenticate({ username: 'hamzachagh@ea.com', password: 'HamzaSF2019'}, function(err, resp){
+  if(!err) console.log('Successfully connected to Salesforce. Cached token: ' + org.oauth.access_token);
+  if(err) console.log('Cannot connect to Salesforce: ' + err);
+});
 
 
 module.exports = org;var nforce = require('nforce');
