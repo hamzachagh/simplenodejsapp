76f6fc5f9752607551e9015dc5de0b637d1aa1ec
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-18 10:32:10 +0000

    ss

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ app.get('/lead', function(req, res , next) {
 
 
 
-      // parse json
+    // parse json
 		var jsonParsed = JSON.parse(JSON.stringify(results.records));
 		// access elements
     console.log("s office phone number is " + jsonParsed[0].name);
-
-
-    
     //console.log('test results --------------- '+JSON.stringify(results.records));
       res.render('pages/lead', { records: JSON.stringify(results.records) , leads:jsonParsed });
     }); 
