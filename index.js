80b8dd6a12636fdbc3396501214711e5b4d0cb8c
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-18 09:43:04 +0000

    dsds

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ const PORT = process.env.PORT || 5000
 const org = require('./sf.js');
 
 var app = express();
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.set('views', path.join(__dirname, 'views'))
-  app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('pages/index'))
-  app.get('/welcome', (req, res) => res.render('pages/welcome',{user:"Hamza CHAGH"}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/welcome', (req, res) => res.render('pages/welcome',{user:"Hamza CHAGH"}))
 
-  
-  
-  
+app.get('/lead', function(req, res , next) {
 
-  app.get('/lead', function(req, res , next) {
+  org.query({ query: "SELECT Id, name FROM Lead limit 5" , raw : true})
+    .then(function(results){
 
-    org.query({ query: "SELECT Id, name FROM Lead limit 5" , raw : true})
-      .then(function(results){
-        console.log('test results --------------- '+JSON.stringify(results.records));
-        res.render('pages/lead', { records: JSON.stringify(results.records) });
-      });
-  
-  });
 
-  app.listen(PORT, () => console.log('Listening on ${ PORT }'))
+      // parse json
+		var jsonParsed = JSON.parse(results);
+
+		// access elements
+    console.log("s office phone number is " + jsonParsed.results[0]);
+    
+    console.log('test results --------------- '+JSON.stringify(results.records));
+      res.render('pages/lead', { records: JSON.stringify(results.records) });
+    });
+    
+});
+
+app.listen(PORT, () => console.log('Listening on ${ PORT }'))
--- sf.js
+++ sf.js
@@ -22,4 +22,28 @@ org.authenticate({ username: 'hamzachagh2@gmail.com', password: 'Hamza_SE2018'},
 
 
 
+module.exports = org;var nforce = require('nforce');
+
+
+var org = nforce.createConnection({
+  clientId: '3MVG9Rd3qC6oMalXcM8hVxjOJvhYFvfY_wIr99DZ23Y_nlfVWpJsw1bRzL_eKt6E31PskZRiL5sgdbXsYA3s.',
+  clientSecret: '8971956947311806869',
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  apiVersion: 'v42.0',  // optional, defaults to current salesforce API version
+  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'single', // optional, 'single' or 'multi' user mode, multi default
+  autoRefresh: true
+});
+
+
+
+
+///single-user mode
+org.authenticate({ username: 'hamzachagh2@gmail.com', password: 'Hamza_SE2018'}, function(err, resp){
+  if(!err) console.log('Successfully connected to Salesforce. Cached token: ' + org.oauth.access_token);
+  if(err) console.log('Cannot connect to Salesforce: ' + err);
+});
+
+
+
 module.exports = org;
