e794f04a64ac33f364f7fd49d91725ab44e657cd
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-18 16:05:15 +0000

    ss

--- sf.js
+++ sf.js
@@ -1,6 +1,6 @@
 var nforce = require('nforce');
 
-/* var org = nforce.createConnection({
+ var org = nforce.createConnection({
   clientId: '3MVG9Rd3qC6oMalXcM8hVxjOJvhYFvfY_wIr99DZ23Y_nlfVWpJsw1bRzL_eKt6E31PskZRiL5sgdbXsYA3s.',
   clientSecret: '8971956947311806869',
   redirectUri: 'http://localhost:3000/oauth/_callback',
@@ -8,14 +8,14 @@ var nforce = require('nforce');
   environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
   mode: 'single', // optional, 'single' or 'multi' user mode, multi default
   autoRefresh: true
-}); */
+}); 
 
-/* //single-user mode
+ //single-user mode
 org.authenticate({ username: 'hamzachagh2@gmail.com', password: 'Hamza_SE2018'}, function(err, resp){
   if(!err) console.log('Successfully connected to Salesforce. Cached token: ' + org.oauth.access_token);
   if(err) console.log('Cannot connect to Salesforce: ' + err);
-}); */
-
+}); 
+/*
 var org = nforce.createConnection({
   clientId: '3MVG95NPsF2gwOiNLx3MoFuCQffRip3hPC7McxHmQkczbGnjSZCId1pI0oqVy28f1qI2F7ZKApJc7Padj9Cid',
   clientSecret: '1849414258188740820',
@@ -30,6 +30,6 @@ org.authenticate({ username: 'hamzachagh@ea.com', password: 'HamzaSF2019'}, func
   if(!err) console.log('Successfully connected to Salesforce. Cached token: ' + org.oauth.access_token);
   if(err) console.log('Cannot connect to Salesforce: ' + err);
 });
-
+*/
 
 module.exports = org;
